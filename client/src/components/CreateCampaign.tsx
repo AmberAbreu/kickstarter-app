7cc0abc8115d3b5b6fcdda7555ab4ad84dff0118
Amber Abreu <ambermabreu@gmail.com>
2021-12-29 10:05:57 -0500

    Refactor: Create Campaign Form

--- client/src/components/CreateCampaign.tsx
+++ client/src/components/CreateCampaign.tsx
@@ -1,25 +1,71 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import FormGroup from "@mui/material/FormGroup";
 import { Controller, useForm } from "react-hook-form";
 import { TextInput } from "./formComponents/TextInput";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core";
+
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiFormControl-root": {
+      width: "80%",
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+const initialValues = {
+  title: "",
+  description: "",
+  photoUrl: "",
+  status: true,
+  raised: 0,
+};
 
 interface Props {}
 
 export default function CreateCampaign({}: Props): ReactElement {
-  const { handleSubmit, control } = useForm();
+  const [values, setValues] = useState(initialValues);
+  const classes = useStyles();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+    console.log(values);
+  };
   return (
-    <FormGroup>
-      <TextInput name={"title"} label={"title"} control={control}></TextInput>
-      <TextInput
-        name={"description"}
-        label={"description"}
-        control={control}
-      ></TextInput>
-      <TextInput
-        name={"photoUrl"}
-        label={"photoUrl"}
-        control={control}
-      ></TextInput>
-    </FormGroup>
+    <form className={classes.root}>
+      <Grid container>
+        <Grid item xs={6}>
+          <TextField
+            variant="outlined"
+            label="title"
+            name="title"
+            value={values.title}
+            onChange={handleInputChange}
+          />
+          <TextField
+            variant="outlined"
+            label="description"
+            name="description"
+            value={values.description}
+            onChange={handleInputChange}
+          />
+          <TextField
+            variant="outlined"
+            name="photoUrl"
+            label="photoUrl"
+            value={values.photoUrl}
+            onChange={handleInputChange}
+          />
+        </Grid>
+        <Grid item xs={6}></Grid>
+      </Grid>
+    </form>
   );
 }
