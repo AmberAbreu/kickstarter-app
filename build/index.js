272da78743ef7549b553f5bc97b76ea17bc2aab9
Amber Abreu <ambermabreu@gmail.com>
2022-01-04 15:01:50 -0500

    Heroku

--- build/index.js
+++ build/index.js
@@ -98,9 +98,9 @@ var generateResponse = function (intent) {
     }
 };
 app.use(express_1["default"].static(path.resolve(__dirname, "../client/public")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../client/public", "index.html"));
-// });
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/public"), "index.html");
+});
 var port = process.env.PORT || 3001;
 app.listen(port, function () {
     return console.log("REST API server ready at: http://localhost:3000");
--- index.ts
+++ index.ts
@@ -57,9 +57,9 @@ const generateResponse = (intent) => {
 
 app.use(express.static(path.resolve(__dirname, "../client/public")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../client/public", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/public"), "index.html");
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () =>
