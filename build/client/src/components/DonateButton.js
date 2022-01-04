dfa85829977152009b66fc8da583b1af8ddd612d
Amber Abreu <ambermabreu@gmail.com>
2022-01-04 17:09:05 -0500

    Heroku

--- build/client/src/components/DonateButton.js
+++ build/client/src/components/DonateButton.js
@@ -1,6 +1,9 @@
 "use strict";
-// import React, { ReactElement, useState } from "react";
-// import Button from "@material-ui/core/Button";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var react_1 = __importDefault(require("react"));
 // import { stripePaymentMethodHandler } from "../script";
 // import {
 //   useStripe,
@@ -30,124 +33,54 @@
 //     },
 //   },
 // };
-// export default function DonateButton({
-//   amount,
-//   setPaymentCompleted,
-// }: Props): ReactElement {
-//   const [loading, setLoading] = useState(false);
-//   const [errorMsg, setErrorMsg] = useState("");
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const stripe = useStripe();
-//   const elements = useElements();
-//   const handleSubmit = async (event: any) => {
-//     event.preventDefault();
-//     if (!stripe || !elements) {
-//       return;
-//     }
-//     setLoading(true);
-//     setErrorMsg("");
-//     const paymentMethodObj = {
-//       type: "card",
-//       card: elements.getElement(CardNumberElement),
-//       billing_details: {
-//         name,
-//         email,
-//       },
-//     };
-//     const paymentMethodResult = await stripe.createPaymentMethod(
-//       paymentMethodObj
-//     );
-//     stripePaymentMethodHandler(
-//       {
-//         result: paymentMethodResult,
-//         amount: amount,
-//       },
-//       handleResponse
-//     );
-//   };
-//   const handleResponse = (response: any) => {
-//     setLoading(false);
-//     if (response.error) {
-//       setErrorMsg(
-//         typeof response.error === "string"
-//           ? response.error
-//           : response.error.message
-//       );
-//       return;
-//     }
-//     setPaymentCompleted(response.success ? true : false);
-//   };
-//   return (
-//     <React.Fragment>
-//       <h4 className="d-flex justify-content-between align-items-center mb-3">
-//         <span className="text-muted">Pay with card</span>
-//       </h4>
-//       <form onSubmit={handleSubmit}>
-//         <div className="row">
-//           <div className="col-md-6 mb-3">
-//             <label htmlFor="cc-name">Name on card</label>
-//             <input
-//               id="cc-name"
-//               type="text"
-//               className="form-control"
-//               value={name}
-//               onChange={(e) => setName(e.target.value)}
-//             />
-//           </div>
-//           <div className="col-md-6 mb-3">
-//             <label htmlFor="cc-email">Email</label>
-//             <input
-//               id="cc-email"
-//               type="text"
-//               className="form-control"
-//               value={email}
-//               onChange={(e) => setEmail(e.target.value)}
-//             />
-//           </div>
-//         </div>
-//         <div className="row">
-//           <div className="col-md-12 mb-3">
-//             <label htmlFor="cc-number">Card Number</label>
-//             <CardNumberElement
-//               id="cc-number"
-//               className="form-control"
-//               options={CARD_ELEMENT_OPTIONS}
-//             />
-//           </div>
-//         </div>
-//         <div className="row">
-//           <div className="col-md-6 mb-3">
-//             <label htmlFor="expiry">Expiration Date</label>
-//             <CardExpiryElement
-//               id="expiry"
-//               className="form-control"
-//               options={CARD_ELEMENT_OPTIONS}
-//             />
-//           </div>
-//           <div className="col-md-6 mb-3">
-//             <label htmlFor="cvc">CVC</label>
-//             <CardCvcElement
-//               id="cvc"
-//               className="form-control"
-//               options={CARD_ELEMENT_OPTIONS}
-//             />
-//           </div>
-//         </div>
-//         <hr className="mb-4" />
-//         <Button variant="contained" type="submit" disabled={loading}>
-//           {loading ? (
-//             <div
-//               className="spinner-border spinner-border-sm text-light"
-//               role="status"
-//             ></div>
-//           ) : (
-//             `PAY ₹${amount}`
-//           )}
-//         </Button>
-//         {errorMsg && <div className="text-danger mt-2">{errorMsg}</div>}
-//       </form>
-//     </React.Fragment>
-//   );
-// }
+function DonateButton() {
+    //   amount,
+    //   setPaymentCompleted,
+    //   const [loading, setLoading] = useState(false);
+    //   const [errorMsg, setErrorMsg] = useState("");
+    //   const [name, setName] = useState("");
+    //   const [email, setEmail] = useState("");
+    //   const stripe = useStripe();
+    //   const elements = useElements();
+    //   const handleSubmit = async (event: any) => {
+    //     event.preventDefault();
+    //     if (!stripe || !elements) {
+    //       return;
+    //     }
+    //     setLoading(true);
+    //     setErrorMsg("");
+    //     const paymentMethodObj = {
+    //       type: "card",
+    //       card: elements.getElement(CardNumberElement),
+    //       billing_details: {
+    //         name,
+    //         email,
+    //       },
+    //     };
+    //     const paymentMethodResult = await stripe.createPaymentMethod(
+    //       paymentMethodObj
+    //     );
+    //     stripePaymentMethodHandler(
+    //       {
+    //         result: paymentMethodResult,
+    //         amount: amount,
+    //       },
+    //       handleResponse
+    //     );
+    //   };
+    //   const handleResponse = (response: any) => {
+    //     setLoading(false);
+    //     if (response.error) {
+    //       setErrorMsg(
+    //         typeof response.error === "string"
+    //           ? response.error
+    //           : response.error.message
+    //       );
+    //       return;
+    //     }
+    //     setPaymentCompleted(response.success ? true : false);
+    //   };
+    return react_1["default"].createElement(react_1["default"].Fragment, null);
+}
+exports["default"] = DonateButton;
 //# sourceMappingURL=DonateButton.js.map
--- client/src/components/DonateButton.tsx
+++ client/src/components/DonateButton.tsx
@@ -1,5 +1,5 @@
-// import React, { ReactElement, useState } from "react";
-// import Button from "@material-ui/core/Button";
+import React, { ReactElement, useState } from "react";
+import Button from "@material-ui/core/Button";
 
 // import { stripePaymentMethodHandler } from "../script";
 
@@ -34,135 +34,61 @@
 //   },
 // };
 
-// export default function DonateButton({
+export default function DonateButton(): ReactElement {
 //   amount,
 //   setPaymentCompleted,
-// }: Props): ReactElement {
-//   const [loading, setLoading] = useState(false);
-//   const [errorMsg, setErrorMsg] = useState("");
+  //   const [loading, setLoading] = useState(false);
+  //   const [errorMsg, setErrorMsg] = useState("");
 
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
+  //   const [name, setName] = useState("");
+  //   const [email, setEmail] = useState("");
 
-//   const stripe = useStripe();
-//   const elements = useElements();
+  //   const stripe = useStripe();
+  //   const elements = useElements();
 
-//   const handleSubmit = async (event: any) => {
-//     event.preventDefault();
+  //   const handleSubmit = async (event: any) => {
+  //     event.preventDefault();
 
-//     if (!stripe || !elements) {
-//       return;
-//     }
+  //     if (!stripe || !elements) {
+  //       return;
+  //     }
 
-//     setLoading(true);
-//     setErrorMsg("");
+  //     setLoading(true);
+  //     setErrorMsg("");
 
-//     const paymentMethodObj = {
-//       type: "card",
-//       card: elements.getElement(CardNumberElement),
-//       billing_details: {
-//         name,
-//         email,
-//       },
-//     };
-//     const paymentMethodResult = await stripe.createPaymentMethod(
-//       paymentMethodObj
-//     );
+  //     const paymentMethodObj = {
+  //       type: "card",
+  //       card: elements.getElement(CardNumberElement),
+  //       billing_details: {
+  //         name,
+  //         email,
+  //       },
+  //     };
+  //     const paymentMethodResult = await stripe.createPaymentMethod(
+  //       paymentMethodObj
+  //     );
 
-//     stripePaymentMethodHandler(
-//       {
-//         result: paymentMethodResult,
-//         amount: amount,
-//       },
-//       handleResponse
-//     );
-//   };
+  //     stripePaymentMethodHandler(
+  //       {
+  //         result: paymentMethodResult,
+  //         amount: amount,
+  //       },
+  //       handleResponse
+  //     );
+  //   };
 
-//   const handleResponse = (response: any) => {
-//     setLoading(false);
-//     if (response.error) {
-//       setErrorMsg(
-//         typeof response.error === "string"
-//           ? response.error
-//           : response.error.message
-//       );
-//       return;
-//     }
-//     setPaymentCompleted(response.success ? true : false);
-//   };
+  //   const handleResponse = (response: any) => {
+  //     setLoading(false);
+  //     if (response.error) {
+  //       setErrorMsg(
+  //         typeof response.error === "string"
+  //           ? response.error
+  //           : response.error.message
+  //       );
+  //       return;
+  //     }
+  //     setPaymentCompleted(response.success ? true : false);
+  //   };
 
-//   return (
-//     <React.Fragment>
-//       <h4 className="d-flex justify-content-between align-items-center mb-3">
-//         <span className="text-muted">Pay with card</span>
-//       </h4>
-//       <form onSubmit={handleSubmit}>
-//         <div className="row">
-//           <div className="col-md-6 mb-3">
-//             <label htmlFor="cc-name">Name on card</label>
-//             <input
-//               id="cc-name"
-//               type="text"
-//               className="form-control"
-//               value={name}
-//               onChange={(e) => setName(e.target.value)}
-//             />
-//           </div>
-//           <div className="col-md-6 mb-3">
-//             <label htmlFor="cc-email">Email</label>
-//             <input
-//               id="cc-email"
-//               type="text"
-//               className="form-control"
-//               value={email}
-//               onChange={(e) => setEmail(e.target.value)}
-//             />
-//           </div>
-//         </div>
-
-//         <div className="row">
-//           <div className="col-md-12 mb-3">
-//             <label htmlFor="cc-number">Card Number</label>
-//             <CardNumberElement
-//               id="cc-number"
-//               className="form-control"
-//               options={CARD_ELEMENT_OPTIONS}
-//             />
-//           </div>
-//         </div>
-
-//         <div className="row">
-//           <div className="col-md-6 mb-3">
-//             <label htmlFor="expiry">Expiration Date</label>
-//             <CardExpiryElement
-//               id="expiry"
-//               className="form-control"
-//               options={CARD_ELEMENT_OPTIONS}
-//             />
-//           </div>
-//           <div className="col-md-6 mb-3">
-//             <label htmlFor="cvc">CVC</label>
-//             <CardCvcElement
-//               id="cvc"
-//               className="form-control"
-//               options={CARD_ELEMENT_OPTIONS}
-//             />
-//           </div>
-//         </div>
-
-//         <hr className="mb-4" />
-//         <Button variant="contained" type="submit" disabled={loading}>
-//           {loading ? (
-//             <div
-//               className="spinner-border spinner-border-sm text-light"
-//               role="status"
-//             ></div>
-//           ) : (
-//             `PAY ₹${amount}`
-//           )}
-//         </Button>
-//         {errorMsg && <div className="text-danger mt-2">{errorMsg}</div>}
-//       </form>
-//     </React.Fragment>
-//   );
-// }
+  return <React.Fragment></React.Fragment>;
+}
